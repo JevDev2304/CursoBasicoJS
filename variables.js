5861258ba3c17971aa769d0783744db6da7f3718
Juanes <jvaldes245@soyudemedellin.edu.co>
2022-12-19 15:52:03 -0500

    Clase valores-variables-funciones

---
+++ variables.js
@@ -0,0 +1,21 @@
+var nombre = "Juan Esteban Valdés Ospina";
+
+// declarar
+var edad;
+
+edad
+// inicializar
+edad = 30;
+
+edad
+
+var elementos = ["Computadora","Celular"];
+
+var persona ={
+    nombre: "Diego",
+    edad:30
+}
+
+persona;
+
+
